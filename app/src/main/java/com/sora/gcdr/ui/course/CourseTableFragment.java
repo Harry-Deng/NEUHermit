f133256cb8311608edbc2497c40caa7fd8577c43
Sora <171139153@qq.com>
2022-06-01 00:51:45 +0800

    简易朋友圈结构

---
+++ app/src/main/java/com/sora/gcdr/ui/course/CourseTableFragment.java
@@ -0,0 +1,38 @@
+package com.sora.gcdr.ui.course;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sora.gcdr.R;
+
+public class CourseTableFragment extends Fragment {
+
+    private CourseTableViewModel mViewModel;
+
+    public static CourseTableFragment newInstance() {
+        return new CourseTableFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_course_table, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CourseTableViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/sora/gcdr/ui/course/CourseTableViewModel.java
@@ -0,0 +1,7 @@
+package com.sora.gcdr.ui.course;
+
+import androidx.lifecycle.ViewModel;
+
+public class CourseTableViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
